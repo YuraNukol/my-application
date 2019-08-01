bdee29d73d7ac5ad829f73d5f56a41d384255d60
n-ura <uranikol77@gmail.com>
2019-08-01 13:59:34 +0300

    Cheked list v.2

--- app/src/main/java/com/example/check_list/MainActivity.java
+++ app/src/main/java/com/example/check_list/MainActivity.java
@@ -5,18 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnCheck;
-    TextView tvCheck, tvList;
+    TextView tvCheck;
+    EditText editList;
 
-    String mas [] = {"{","}","}","{","}","{"};
-   //String s="{,},{,{,},}";
-
-    int d1=0,d2=0;
     boolean boll=true;
 
     @Override
@@ -25,40 +23,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         tvCheck = findViewById(R.id.tvCheck);
-        tvList = findViewById(R.id.tvList);
+        editList = findViewById(R.id.editList);
 
-        for(int i = 0; i<mas.length;i++){
-
-            String s = String.valueOf(tvList.getText());
-            tvList.setText(s+ mas[i]);
-
-        }
         btnCheck = findViewById(R.id.btnCheck);
         btnCheck.setOnClickListener(this);
-
-
     }
 
     @Override
     public void onClick(View view) {
 
-        //ArrayList<String> masCheck = new ArrayList<String>(Arrays.asList(s.split(",")));
+        int d1=0,d2=0;
 
-        String s = String.valueOf(tvCheck.getText());
+        String s1 = String.valueOf(editList.getText());
+        char [] s2 = s1.toCharArray();
 
-        for (int i=0; i<mas.length;i++){
 
-            if(mas[i].equals("{")) d1++;
-            if(mas[i].equals("}")) d2++;
+        for (int i=0; i<s2.length;i++){
+
+            if(String.valueOf(s2[i]).equals("{")) d1++;
+            if(String.valueOf(s2[i]).equals("}")) d2++;
 
             if(d1<d2){
-                tvCheck.setText(s + " false");
+                tvCheck.setText("Результат: false");
                 boll=false;
             }
+            if(d1>d2) boll=true;
         }
         if(boll==true){
-            if(d1==d2) tvCheck.setText(s + " true");
-            else tvCheck.setText(s + " false");
+            if(d1==d2) tvCheck.setText("Результат: true");
+            else tvCheck.setText("Результат: false");
         }
     }
 
